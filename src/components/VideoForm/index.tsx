8f7bc1edbf5d633f6249bab02374e3e283870c47
BlackKingWhitePawn <team109team@gmail.com>
2023-06-24 00:12:09 +0500

    change file loading

--- src/components/VideoForm/index.tsx
+++ src/components/VideoForm/index.tsx
@@ -1,29 +1,56 @@
 import axios from "axios";
 import { useState } from "react";
 import { API } from "utils/config";
+import { } from "@mui/material/Tooltip";
 
 function VideoForm() {
-  const [file, setFile] = useState<any>(null);
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    const formData = new FormData();
-    formData.append('video', file);
-    axios.post('/upload-video', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data'
+  const [file, setfile] = useState<any>(null)
+
+  function handleFileChange(event: any) {
+    const videoFile = event.target.files[0]
+    setfile(videoFile)
+  }
+
+  async function handleSubmit(event: any) {
+    const CHUNK_SIZE = 5242880; // Размер чанка (в байтах) - здесь установлено 5 МБ
+
+    const totalChunks = Math.ceil(file.size / CHUNK_SIZE);
+    let currentChunk = 0;
+
+    while (currentChunk < totalChunks) {
+      const start = currentChunk * CHUNK_SIZE;
+      const end = Math.min(start + CHUNK_SIZE, file.size);
+      const chunk = file.slice(start, end);
+
+      const formData = new FormData();
+      formData.append('video_id', '-1')
+      formData.append('chunk', chunk);
+      formData.append('chunk_id', `${currentChunk}`)
+      if (currentChunk = totalChunks - 1) {
+        formData.append('complete', 'true')
+      } else {
+        formData.append('complete', 'false')
       }
-    })
-      .then(response => {
-        console.log('Видео успешно загружено');
-        // выполняем нужные действия
-      })
-      .catch(error => {
-        console.error(`Ошибка загрузки видео: ${error}`);
-      });
-  };
-  const handleFileChange = (event: any) => {
-    setFile(event.target.files[0]);
-  };
+      // formData.append('totalChunks', totalChunks);
+      // formData.append('currentChunk', file);
+
+      try {
+        await axios.post('/upload-video', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        currentChunk++;
+      } catch (error) {
+        console.error('Error uploading chunk:', error);
+        // Обработка ошибки при загрузке чанка
+      }
+    }
+
+    // Все чанки загружены
+    console.log('Video upload complete!');
+  }
 
   return (
     <form onSubmit={handleSubmit}>
